77919cabb61fd371f0985770b1902982ddc0522b
nemo-tj <sa514015@mail.ustc.edu.cn>
2020-08-15 09:36:25 +0800

    main

--- main/main.cc
+++ main/main.cc
@@ -5,8 +5,5 @@ int main(int argc, char ** argv) {
   Runner::GetInstance().run_link_list();
   Runner::GetInstance().run_priority_heap();
   Runner::GetInstance().run_sorter();
-  for (int i = 0; i < -1; ++i) {
-    std::cout << "no kidding"<< std::endl;
-  }
   return 0;
 }
